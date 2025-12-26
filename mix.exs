f075bee42164d8b67bb8cf1f4c3bc4a90686f862
Iskandar Rizki <iskandar.rizki@gmail.com>
2025-12-26 23:52:17 +0700

    add license

--- mix.exs
+++ mix.exs
@@ -7,14 +7,25 @@ defmodule ExVoix.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
+
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Iskandar Rizki"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "github.com"}
     ]
   end
 
